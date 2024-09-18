48b050eb3d5815a59056fef01773b0a36e228a4c
Kenzaki_Kazuma(剣崎 一真) <138582489+Oaiker123@users.noreply.github.com>
2024-09-18 23:22:19 +0700

    oai-commit-update-users

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -10,8 +10,13 @@ class AdminAuthenticate
 {
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->hasRole(1)) { // Kiểm tra với vai trò là 1
-            return $next($request);
+        if (Auth::check()) {
+            info('User ID: ' . Auth::id());
+            info('User Roles: ', Auth::user()->roles()->pluck('role_id')->toArray());
+
+            if (Auth::user()->hasRole(1)) { // Kiểm tra với vai trò là 1
+                return $next($request);
+            }
         }
 
         return redirect()->route('admin.login'); // Chuyển hướng nếu không phải admin
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,81 +11,47 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
         'address',
         'phone',
-        'image',
-        'role_id'
+        'image'
     ];
 
-    /**
-     * Mối quan hệ với model Role.
-     */
-    public function role()
+    public function roles()
     {
-        return $this->belongsTo(Role::class);
+        return $this->belongsToMany(Role::class, 'user_roles', 'user_id', 'role_id');
     }
 
-    /**
-     * Mối quan hệ với model Order.
-     */
     public function orders()
     {
         return $this->hasMany(Order::class);
     }
 
-    /**
-     * Mối quan hệ với model Post.
-     */
     public function posts()
     {
         return $this->hasMany(Post::class);
     }
 
-    /**
-     * Mối quan hệ với model Comment.
-     */
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
-    /**
-     * Kiểm tra vai trò của người dùng.
-     *
-     * @param mixed $role
-     * @return bool
-     */
-    public function hasRole($role)
+    public function hasRole($roleId)
     {
-        return $this->role->id == $role; // Kiểm tra vai trò dựa trên id
+        return $this->roles()->where('role_id', $roleId)->exists();
     }
 }
