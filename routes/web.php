cbf6cdef3416347ee88017825782177d9deb1ed3
datpink <datpink04@gmail.com>
2024-09-19 09:51:27 +0700

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,17 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CatalogueController;
 use App\Http\Controllers\Admin\OrderController;
+
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\CategoryController;
+
+// use App\Http\Controllers\Auth\LoginController;
+// use App\Http\Controllers\Auth\RegisterController;
+// use App\Http\Controllers\Client\ClientController;
+// use App\Http\Controllers\Client\ContactController;
+// use App\Http\Controllers\Client\PostController;
+// use App\Http\Controllers\Client\ProductController; // Thêm controller cho client
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,10 +28,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route đăng nhập
+// Route cho trang chưa đăng nhập
+// Route::prefix('shop')->group(function () {
+//     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+//     Route::post('/login', [LoginController::class, 'login']);
+//     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+//     Route::post('/register', [RegisterController::class, 'register'])->name('register');
+//     Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+//     // Các route không yêu cầu đăng nhập
+//     Route::get('/products', [ProductController::class, 'index'])->name('client.products.index');
+//     Route::get('/blog', [PostController::class, 'index'])->name('client.posts.index');
+//     Route::get('/contact', [ContactController::class, 'index'])->name('client.contact.index');
+// });
+
+// // Route cho trang home không yêu cầu xác thực
+// Route::get('/', [ClientController::class, 'index'])->name('client.index');
+
 // Route đăng nhập
 Route::get('/admin/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
 Route::post('/admin/login', [AdminController::class, 'login'])->name('admin.login.post');
@@ -34,9 +56,9 @@ Route::post('/admin/logout', [AdminController::class, 'logout'])->name('admin.lo
 Route::prefix('admin')->middleware('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.index'); // Bảo vệ route này
 
-    Route::get('/admin/profile', [AdminController::class, 'profile'])->name('admin.profile');
+    Route::get('/profile', [AdminController::class, 'profile'])->name('admin.profile');
 
-    //Route Catalogue
+    // Route Catalogue
     Route::resource('catalogues', CatalogueController::class);
     Route::get('catalogues-trash', [CatalogueController::class, 'trash'])->name('catalogues.trash');
     Route::post('catalogues/{id}/restore', [CatalogueController::class, 'restore'])->name('catalogues.restore');
@@ -48,8 +70,7 @@ Route::prefix('admin')->middleware('admin')->group(function () {
     Route::post('brands/{id}/restore', [BrandController::class, 'restore'])->name('brands.restore');
     Route::delete('brands/{id}/force-delete', [BrandController::class, 'forceDelete'])->name('brands.forceDelete');
 
-
-    //Route Order
+    // Route Order
     Route::resource('orders', OrderController::class);
     Route::get('/orders-trash', [OrderController::class, 'trash'])->name('orders.trash');
     Route::post('/orders/{id}/restore', [OrderController::class, 'restore'])->name('orders.restore');
