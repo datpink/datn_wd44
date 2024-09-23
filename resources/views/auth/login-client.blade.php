0f4834a03d5188c94c524903bf9a540d4ed95e85
Kenzaki_Kazuma(剣崎 一真) <138582489+Oaiker123@users.noreply.github.com>
2024-09-23 12:28:29 +0700

    update-menu-post-cate

--- resources/views/auth/login-client.blade.php
+++ resources/views/auth/login-client.blade.php
@@ -4,28 +4,26 @@
     </a>
     <ul class="sub-menu">
         @if (Auth::check())
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--dashboard is-active">
-                <a href="#">Dashboard</a>
-            </li>
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--orders">
+            @if (Auth::user()->isAdmin())
+                <!-- Check if the user is an admin -->
+                <li
+                    class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--dashboard is-active">
+                    <a href="{{ route('admin.index') }}">Admin Dashboard</a> <!-- Link to admin dashboard -->
+                </li>
+            @endif
+            <li class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--orders">
                 <a href="#">Orders</a>
             </li>
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--downloads">
+            <li class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--downloads">
                 <a href="#">Downloads</a>
             </li>
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--edit-addresses">
+            <li class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--edit-addresses">
                 <a href="#">Addresses</a>
             </li>
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--edit-account">
+            <li class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--edit-account">
                 <a href="#">Account details</a>
             </li>
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--customer-logout">
+            <li class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--customer-logout">
                 <form action="{{ route('logout') }}" method="POST" style="display: inline;">
                     @csrf
                     <button type="submit"
@@ -33,8 +31,7 @@
                 </form>
             </li>
         @else
-            <li
-                class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--login">
+            <li class="menu-item kobolg-MyAccount-navigation-link kobolg-MyAccount-navigation-link--login">
                 <a href="{{ route('login') }}">Đăng Nhập</a>
             </li>
         @endif
