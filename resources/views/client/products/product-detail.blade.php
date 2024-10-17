920b691789bc849226adb75f8dad89d8179b4b92
datpink <datpink04@gmail.com>
2024-10-17 16:21:51 +0700

    update add image when insert excel file

--- resources/views/client/products/product-detail.blade.php
+++ resources/views/client/products/product-detail.blade.php
@@ -32,7 +32,7 @@
                                                     @if ($product->image_url && \Storage::exists($product->image_url))
                                                         <img src="{{ \Storage::url($product->image_url) }}"
                                                             alt="{{ $product->name }}"
-                                                            style="max-width: 100px; height: auto;">
+                                                            style="width: 70%;margin: 0 auto; height: auto;">
                                                     @else
                                                         <p>Không có ảnh</p>
                                                     @endif
